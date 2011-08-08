b679a8197886f228320c9fcf1dd7f6fa810b422a
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2011-08-08 23:32:54 +1000

    ex 13b adds vertical chart output

--- ex1-13_b.c
+++ ex1-13_b.c
@@ -1,5 +1,5 @@
 /* Write a program to print a histogram of the lengths of words in its input. It is easy to draw the histogram with the bars horizontal; a vertical orientation is more challenging. */
-
+// vertical orientation
 #include <stdio.h>
 #define MAX_WORD_LENGTH 100
 
@@ -28,11 +28,11 @@ int main() {
   }
 
   int chartWordLen = 0;
-  //printf("Len | Freq | Chart \n");
-  printf("Len | Chart \n");
+  printf("Len | Freq | Chart \n");
+  //printf("Len | Chart \n");
   for (i=1; i < MAX_WORD_LENGTH; i++) { 
     if ((chartWordLen = wordLen[i]) > 0 ) {
-      printf( "%3u | ",i,chartWordLen);
+      printf( "%3u | %4u | ",i,chartWordLen);
 
       for (; chartWordLen > 0; chartWordLen--){
         putchar('=');
@@ -40,6 +40,38 @@ int main() {
       putchar('\n');
     }
   }
+
+
+  int maxfreq = 0;
+  int maxword = 0;
+  chartWordLen = 0;
+  for (i=0; i < MAX_WORD_LENGTH; i++) { 
+
+    chartWordLen = wordLen[i];
+
+    if ( chartWordLen > 0) {
+      if (chartWordLen > maxfreq) {
+        maxfreq = chartWordLen;
+      }
+      if ( i > maxword ) {
+        maxword = i;
+      }
+    }    
+  }
+  printf("maxfreq :%u\n",maxfreq); 
+  printf("maxword :%u\n",maxword); 
+  int row;
+  int column;
+  for ( row = maxfreq ; row > 0; row-- ) {
+    for ( column = 0; column < maxword ; column++ ){
+      putchar( (wordLen[column] == row ) ? '#' : '_'); 
+    
+    }
+    putchar('\n');
+
+  }
+
+
   
   printf("Total whitespace: %u\n", whitespace);
   
